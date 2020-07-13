859f771bd7f955becf169cd29cbefab06b6a1d5a
phatdroid94 <phatdroid94@gmail.com>
2020-07-13 20:39:08 +0700

    replace , add

--- app/src/main/java/com/example/fragmenttransaction30032020/MainActivity.java
+++ app/src/main/java/com/example/fragmenttransaction30032020/MainActivity.java
@@ -1,16 +1,47 @@
 package com.example.fragmenttransaction30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager mFragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragmentManager = getSupportFragmentManager();
+    }
 
+    public void addAndroid(View view) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.liearlayoutContainer,androidFragment);
+        fragmentTransaction.commit();
+    }
 
+    public void addIos(View view) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.add(R.id.liearlayoutContainer,iosFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceAndroid(View view) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer,androidFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void replaceIos(View view) {
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer,iosFragment);
+        fragmentTransaction.commit();
     }
 }
